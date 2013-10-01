65436e5c7f291d7f3ce4d75990db12ea85f5663a
Ryan Leahy <ryanleahy90@gmail.com>
2013-10-01 16:45:22 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Djsms::Application.config.secret_token = '4bf2e9b7f488b531d7845a587246c742c34bc02423aa8bbc1619f76b892022d760f9014026429c0017fe5c9628ccc22d538da711edf3d20867cf22a0ee5d3642'
