c44666204909f523880f857c497dd9ab4db719a3
Ryan Leahy <ryanleahy90@gmail.com>
2013-10-01 17:35:30 -0700

    user model attributes set up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :username, :email, :password, :password_confirmation
+  has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,15 @@
 require 'spec_helper'
 
 describe User do
-
+	before do
+		@user = User.new(username: "example1", email: "example@me.com",
+										 password: "password", password_confirmation: "password")
+	end
 	subject { @user }
 
 	it { should respond_to(:username) }
+	it { should respond_to(:email) }
 	it { should respond_to(:password_digest) }
-	it { should respond_to(:password_password) }
+	it { should respond_to(:password) }
 	it { should respond_to(:password_confirmation) }
 end
