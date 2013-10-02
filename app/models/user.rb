1f0d776d2b9a2f2c05f6c37ef6f9219f86471ee9
Ryan Leahy <ryanleahy90@gmail.com>
2013-10-01 17:46:08 -0700

    red green refactored for username validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :username, :email, :password, :password_confirmation
   has_secure_password
+
+  validates :username, presence: true, length: { maximum: 20 }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,4 +12,14 @@ describe User do
 	it { should respond_to(:password_digest) }
 	it { should respond_to(:password) }
 	it { should respond_to(:password_confirmation) }
+
+	describe "when username is not present" do
+		before { @user.username = "" }
+		it { should_not be_valid }
+	end
+
+	describe "when username is too long" do
+		before { @user.username = "x" * 21 }
+		it { should_not be_valid}
+	end
 end
