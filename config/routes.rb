579a278f4d9d6386dc1ab14d1bbeb7f22fb3ac9f
Ryan Leahy <ryanleahy90@gmail.com>
2013-10-07 19:04:09 -0700

    modified routes to have signin go through sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Djsms::Application.routes.draw do
   resources :users
-  root to: 'users#login'
-  match '/signin', to: 'users#login'
+  resources :sessions, only: [:new, :create, :destroy]
+  root to: 'sessions#new'
+  match '/signin', to: 'sessions#new', 			via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
 end
