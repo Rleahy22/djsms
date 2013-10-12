8c23bcf070a7c692849558635ecdafd859d5c06a
Ryan Leahy <ryanleahy90@gmail.com>
2013-10-11 18:46:23 -0700

    moved features specs into requests folder

---
+++ spec/requests/authentication_pages_spec.rb
@@ -0,0 +1,89 @@
+require 'spec_helper'
+
+describe "Authentication" do
+
+	subject { page }
+
+	describe "sign in page" do
+		before { visit signin_path }
+
+		it { should have_selector("input[type=submit][value='Sign In']") }
+		it { should have_link('Sign Up') }
+	end
+
+	describe "sign in" do
+		before { visit signin_path }
+
+		describe "with invalid information" do
+			before { click_button "Sign In" }
+
+			it { should have_selector("input[type=submit][value='Sign In']") }
+			it { should have_content('Invalid') }
+		end
+
+		describe "with valid information" do
+			let(:user) { FactoryGirl.create(:user) }
+			before { sign_in user }
+
+			it { should have_link(user.username)}
+			it { should have_link('Sign Out')}
+			it { should have_selector('h1', text: user.username) }
+			it { should have_link('Account Settings') }
+		
+			describe "followed by signout" do
+				before { click_link "Sign Out" }
+				it { should have_selector("input[type=submit][value='Sign In']") }
+			end
+		end
+	end
+
+	describe "authorization" do
+		describe "for non-signed-in users" do
+			let(:user) { FactoryGirl.create(:user) }
+
+			describe "when attempting to visit a protected page" do
+				before do
+					visit edit_user_path(user)
+					fill_in "EMAIL", 		with: user.email
+					fill_in "PASSWORD", with: user.password
+					click_button "Sign In"
+				end
+
+				describe "after signing in" do
+					it "should render the desired protected page" do
+						page.should have_content('Update Your Profile')
+					end
+				end
+			end
+
+			describe "in the Users controller" do
+
+				describe "visiting the edit page" do
+					before { visit edit_user_path(user) }
+					it { should have_selector("input[type=submit][value='Sign In']") }
+				end
+				describe "submitting to the update action" do
+				  before { put user_path(user) }
+				  specify { response.should redirect_to(signin_path) }
+				end
+			end
+		end
+
+		describe "as wrong user" do
+			let(:user) { FactoryGirl.create(:user) }
+			let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@user.com", 
+																									 username: "Wronguser") }
+			before { sign_in user }
+
+			describe "visiting Users#edit page" do
+				before { visit edit_user_path(wrong_user) }
+				it { should_not have_content('Update Your Profile') }
+			end
+
+			describe "submitting a PUT request to the Users#update action" do
+				before { put user_path(wrong_user) }
+				specify { response.should redirect_to(signin_path) }
+			end
+		end
+	end
+end
---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,92 @@
+require 'spec_helper'
+
+describe "UserPages" do
+ 
+	subject { page }
+
+	describe "index" do
+		before { visit users_path }
+
+		it { should have_selector('h1', text: "All Users") }
+
+		describe "pagination" do
+
+			before(:all) { 30.times { FactoryGirl.create(:user) } }
+			after(:all) { User.delete_all }
+
+			it "should list each user" do
+				User.paginate(page: 1).each do |user|
+					page.should have_selector('li', text: user.username)
+				end
+			end
+		end
+	end
+
+ 	describe "signup" do
+ 		before { visit new_user_path }
+
+ 		it { should have_selector("input[type=submit][value='Sign Up']") }
+
+ 		describe "with invalid information" do
+ 			it "should not create a user" do
+ 				expect { click_button "Sign Up" }.not_to change(User, :count)
+ 			end
+
+ 			describe "after submission" do
+ 				before { click_button "Sign Up" }
+
+ 				it { should have_selector("input[type=submit][value='Sign Up']") }
+ 				it { should have_content('*') }
+ 			end
+ 		end
+
+ 		describe "with valid information" do
+ 			before do
+ 				fill_in "Username", with: "Example1"
+ 				fill_in "Email", with: "example@me.com"
+ 				fill_in "Password", with: "Password1"
+ 				fill_in "Confirm Password", with: "Password1"
+ 			end
+
+ 			describe "after saving the user" do
+ 				before { click_button "Sign Up" }
+ 				let(:user) { User.find_by_email("example@me.com")}
+
+ 				it { should have_selector('h1', text: user.username) }
+ 				it { should have_link('Sign Out') }
+ 			end
+
+ 			it "should create a user" do
+ 				expect { click_button "Sign Up" }.to change(User, :count).by(1)
+ 			end
+ 		end
+ 	end
+
+ 	describe "edit" do
+ 		let(:user) { FactoryGirl.create(:user) }
+ 		before do
+ 			sign_in user
+ 			visit edit_user_path(user)
+ 		end
+
+ 		describe "page" do
+ 			it { should have_content("Update Your Profile") }
+ 			it { should have_link("Delete Account") }
+ 		end
+
+ 		describe "with valid information" do
+ 			let(:new_username) { "NewUserName" }
+ 			let(:new_email) { "new@email.com" }
+ 			before do
+ 				fill_in "Username", with: new_username
+ 				fill_in "Email", with: new_email
+ 				fill_in "Password", with: user.password
+ 				fill_in "Password Confirmation", with: user.password
+ 				click_button "Save Changes"
+ 			end
+
+ 			specify { expect(user.reload.username).to eq new_username }
+ 			specify { expect(user.reload.email).to eq new_email }
+ 		end
+ 	end
+end
