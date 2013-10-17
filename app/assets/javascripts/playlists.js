7722a8cdd6af20f1ce5a38bb408bfcc1abe8b285
Ryan Leahy <ryanleahy90@gmail.com>
2013-10-16 21:33:47 -0700

    playlist show view works with rdio_api to start on page load with correct source

--- app/assets/javascripts/playlists.js
+++ app/assets/javascripts/playlists.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+	var activePlaylist = {}
+
 	var newPlaylist = function(name) {
 		R.request({
 			method: "createPlaylist",
@@ -28,9 +30,34 @@ $(document).ready(function() {
 		)
 	}
 
+	var searchRdio = function(query) {
+		R.request({
+			method: "search",
+			content: {
+				query: query,
+				types: "track"
+			},
+			success: function(response) {
+				console.log(response.result.results[0])
+			}
+		})
+	}
+
 	var togglePlayPause = function() {
 		$('.play-visible').toggle()
 		$('.pause-visible').toggle()
+		R.player.togglePause()
+	}
+
+	var initPlayer = function() {
+		if ($('.player').length != 0) {
+			R.ready(function() {
+				R.player.play({source: $('.playlist-data').data('playlistid')})
+				activePlaylist = R.player.playingSource()
+				console.log(activePlaylist.attributes.tracks.models)
+				setTimeout(function() {R.player.togglePause()}, 500)
+			})
+		}
 	}
 
 	$('.create-playlist').on('click', function(e) {
@@ -40,11 +67,41 @@ $(document).ready(function() {
 
 	$('.pause-visible').toggle()
 
-	$('.play-visible').on('click', function(){
+	$('.play-visible').on('click', function() {
 		togglePlayPause()
 	})
 
-	$('.pause-visible').on('click', function(){
+	$('.pause-visible').on('click', function() {
 		togglePlayPause()
 	})
+
+	$('.previous').on('click', function() {
+		R.player.previous()
+		var currentSong = $('.playing')
+		// if (R.player.playingTrack().attributes.key == currentSong.previous().data('key'))
+		currentSong.removeClass("playing")
+		currentSong.prev().addClass("playing")
+	})
+
+	$('.next').on('click', function() {
+		R.player.next()
+		var currentSong = $('.playing')
+		currentSong.removeClass("playing")
+		currentSong.next().addClass("playing")
+	})
+
+	$('.view-playlist').on('click', function(e) {
+		e.preventDefault()
+		var playlistUrl = $(this).attr('href')
+		$.get(playlistUrl, function(data) {
+			$('.container').remove()
+			$('body').append(data)
+		})
+	})
+
+	setTimeout(initPlayer, 2000)
+
+	// R.player.play({source: $('.playlist-data').data('playlistid')})
+
+	$('#song-list').append('<li class="playlist-song playing"><img src="http://rdio-b.cdn3.rdio.com/album/2/8/4/000000000032d482/5/square-200.jpg" class="song-icon"><h3 class="song-info">Katy Perry - Roar</h3></li>')
 })
