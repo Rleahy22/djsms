cabdd78a0f7eb7dfe7c36b787d992c6f203cd462
Ryan Leahy <ryanleahy90@gmail.com>
2013-10-27 01:49:25 -0700

    removed twilio helper method

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -40,10 +40,4 @@ module SessionsHelper
 	def store_location
 		session[:return_to] = request.url
 	end
-
-	# def generate_twilio
-	# 	twilio_account_sid = ENV['TWILIO_ACCOUNT_SID']
-	#   twilio_auth_token = ENV['TWILIO_AUTH_TOKEN']
-	#   client = Twilio::REST::Client.new twilio_account_sid, twilio_auth_token
-	# end
 end
